30046e805867d283c0ccf370d5e9a7bd725b6b35
Mateu <mateusantonioofc@gmail.com>
2025-09-03 21:02:58 -0300

    create Emprestimos entity

--- src/main/java/com/github/mateusantonioofc/chrono/model/EmprestimosEntity.java
+++ src/main/java/com/github/mateusantonioofc/chrono/model/EmprestimosEntity.java
@@ -2,6 +2,9 @@ package com.github.mateusantonioofc.chrono.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "emprestados")
 public class EmprestimosEntity {
@@ -9,4 +12,59 @@ public class EmprestimosEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String nome;
+    private LocalDate data;
+    private BigDecimal valor;
+    private String descricao;
+
+    public EmprestimosEntity() {
+    }
+
+    public EmprestimosEntity(Long id, String nome, LocalDate data, BigDecimal valor, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.data = data;
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
