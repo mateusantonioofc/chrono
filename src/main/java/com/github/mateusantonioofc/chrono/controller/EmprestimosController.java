b70af938a3843a0bd4b403d1f5e9a17f69fab0b6
Mateu <mateusantonioofc@gmail.com>
2025-09-03 21:41:53 -0300

    create endpoints of Emprestimo Controller

--- src/main/java/com/github/mateusantonioofc/chrono/controller/EmprestimosController.java
+++ src/main/java/com/github/mateusantonioofc/chrono/controller/EmprestimosController.java
@@ -2,10 +2,8 @@ package com.github.mateusantonioofc.chrono.controller;
 
 import com.github.mateusantonioofc.chrono.model.EmprestimosEntity;
 import com.github.mateusantonioofc.chrono.repository.EmprestimosRepository;
-import com.github.mateusantonioofc.chrono.service.EmprestimosService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,34 @@ public class EmprestimosController {
     @GetMapping
     public List<EmprestimosEntity> getAllEmprestimos() {
         return emprestimosRepository.findAll();
+    }
+
+    @PostMapping
+    public EmprestimosEntity createEmprestimo(@RequestBody EmprestimosEntity entity) {
+        return emprestimosRepository.save(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteEmprestimo(@PathVariable Long id) {
+        if(emprestimosRepository.existsById(id)) {
+            emprestimosRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateEmprestimo(@PathVariable Long id, @RequestBody EmprestimosEntity dados) {
+        return emprestimosRepository.findById(id)
+                .map(emprestimo -> {
+                    emprestimo.setNome(dados.getNome());
+                    emprestimo.setData(dados.getData());
+                    emprestimo.setValor(dados.getValor());
+                    emprestimo.setDescricao(dados.getDescricao());
+                    emprestimosRepository.save(emprestimo);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
